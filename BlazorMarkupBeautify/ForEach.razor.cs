a988dc1998cc522de4d1a7892c025c6ddeb6bf88
JacobTonder <jacob@softwarekonsulenten.dk>
2022-01-02 08:32:40 +0100

    ForEach: Added EditorRequired attribute to Collection

--- BlazorMarkupBeautify/ForEach.razor.cs
+++ BlazorMarkupBeautify/ForEach.razor.cs
@@ -5,6 +5,7 @@ namespace BlazorMarkupBeautify
     public partial class ForEach<T> : ComponentBase
     {
         [Parameter]
+        [EditorRequired]
         public IEnumerable<T>? Collection { get; set; }
 
         [Parameter]
