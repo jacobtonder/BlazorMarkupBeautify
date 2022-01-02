9665f387fc7b9faa66b236d0965d8af5b6736e91
JacobTonder <jacob@softwarekonsulenten.dk>
2022-01-02 08:20:16 +0100

    Added Conditional

---
+++ BlazorMarkupBeautify/Conditional.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMarkupBeautify
+{
+    public partial class Conditional : ComponentBase
+    {
+        [Parameter]
+        public bool Condition { get; set; }
+
+        [Parameter]
+        public RenderFragment? True { get; set; }
+
+        [Parameter]
+        public RenderFragment? False { get; set; }
+    }
+}
